d846295a61b769aa85442ac5c591af287bcdf7c3
carob98 <caro.bon98@gmail.com>
2025-08-01 17:54:28 +0200

    Implementazione Modale funzionante per Elimina

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -41,11 +41,40 @@
                                         class="btn btn-warning me-md-2">
                                         Modifica
                                     </a>
-                                    <form action="{{ route('books.destroy', ['book' => $book]) }}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-danger me-md-2">Elimina</button>
-                                    </form>
+                                    <!-- Button trigger modal -->
+                                    <button type="button" class="btn btn-danger me-md-2" data-bs-toggle="modal"
+                                        data-bs-target="#book-{{ $book->id }}">Elimina</button>
+
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="book-{{ $book->id }}" data-bs-backdrop="static"
+                                        data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel"
+                                        aria-hidden="true">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h1 class="modal-title fs-5" id="${this.id}"">
+                                                        Sei sicuro di
+                                                        voler eliminare il libro "{{ $book->name }}"?
+                                                    </h1>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                        aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    Attenzione: L'operazione non é reversibile.
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary"
+                                                        data-bs-dismiss="modal">Annulla</button>
+                                                    <form action="{{ route('books.destroy', ['book' => $book]) }}"
+                                                        method="POST">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit" class="btn btn-danger">Elimina</button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </td>
                         </tr>
